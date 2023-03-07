3f1debc37e70e22dd907e9b8c12bfd890d3a6d18
Laryssa Ferreira <laryssa.oliveira1902@gmail.com>
2023-03-07 09:30:43 -0300

    A

--- javascript/main.js
+++ javascript/main.js
@@ -23,3 +23,30 @@ closeIcon = document.querySelector(".close-icon");
 
 searchIcon.addEventListener("click", () => inputBox.classList.add("open"));
 closeIcon.addEventListener("click", () => inputBox.classList.remove("open"));
+
+function Tablefilter (phrase, _id) {
+  var words = phrase.value.toLowerCase().split(" ");
+  var table = document.getElementById(_id);
+  var ele;
+  for (var r = 0; r < table.rows.length; r++ ) {
+    ele = table.rows[r].innerHTML.replace(/<[^>]+>/g,"");
+    var displayStyle = 'none';
+    for (var i = 0; i < words.length; i++) {
+      if (ele.toLowerCase().indexOf(words[i])>=0) {   displayStyle = '';}
+      else { displayStyle = 'none';  break; }
+    }
+    table.rows[r].style.display = displayStyle;
+  }
+}
+
+function countDisplayedRows(_id)
+{
+      var hiddenRowCount = 0;
+      var table = document.getElementById(_id);
+      var rows = table.getElementsByTagName("tr");
+      for (var i = 0; i < rows.length; i++) {
+        if(rows[i].style.display == 'none') hiddenRowCount++;
+      }
+      shown = rows.length - hiddenRowCount;
+      document.getElementById('DisplayCount').innerHTML = shown;
+}
